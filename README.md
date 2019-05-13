2aea56824415dee703b746ae59e27da35fafeed6
r_formulapro <rabadji.triosoftu@gmail.com>
2019-05-13 19:49:02 +0300

    test

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# MobiChord
+To Istall the app follow next steps:
+
+#Open a terminal/console/command prompt
+
+#clone this repository in the current directory  
+git clone https://github.com/Rabadji/MobiChord.git
+ 
+#change to the directory where you cloned Processing
+cd mobichord
+
+#Install all pacheges by type
+npm install
+
+#In .env file type the directory of zip file that you want to use
+Ex: DIR=C:/Users/User/Desktop/Carrier_Integration_-_Data.zip
+
+#In terminal/console/command prompt type
+node index.js
+
+# in current forlder you will see new file "myjsonfile.json" with all data from .csv files
+
--- index.js
+++ index.js
@@ -1,61 +1,66 @@
-require('dotenv').config();
+require('dotenv').config(); 
 const extract = require('extract-zip')
 const path = require('path');
 const fs = require('fs');
 const csv=require('csvtojson')
 const numberPattern = /\d+/g;
-async function start(){
- await extract(process.env.DIR, {dir: __dirname+'/data/'}, function (err) { })
- const directoryPath = path.join(__dirname, 'data');
- var fullJSON=[]
- await fs.readdir(directoryPath, function (err, files) {
-     if (err) {
-         return console.log('Unable to scan directory: ' + err);
-     }                    
-     files.forEach(function (file) { 
-      csv({
-        delimiter:'||',
-        colParser:{
-          'date':function(item, head, resultRow, row , colIdx){
-            try{var date=new Date(item).toISOString().substring(0, 10);}catch(err){}
-            return date;
-          },
-          'name':function(item, head, resultRow, row , colIdx){
-            return resultRow.last_name+' '+resultRow.first_name;
-          },
-          'phone':function(item, head, resultRow, row , colIdx){
-            return item.match( numberPattern ).join([]) ;
-            
-          },
-          'cc':function(item, head, resultRow, row , colIdx){
-            return item.substring(3, item.length)
-          },
-      },
-      ignoreColumns:/(user|email)/,
-      })
-      .fromFile('./data/'+file)
-      .then((jsonObj)=>{
-          for (var i=0; i<jsonObj.length; i++){
-            jsonObj[i]={
-              "name": jsonObj[i].name,
-              "phone": jsonObj[i].phone,
-              "person": {
-                "firstName": jsonObj[i].first_name,
-                "lastName":jsonObj[i].last_name
-              },
-              "amount": jsonObj[i].amount,
-              "date": jsonObj[i].date,
-              "costCenterNum": jsonObj[i].cc
-            }
-          }
-        fullJSON.push(jsonObj)
-        fs.writeFile('./data/myjsonfile.json',fullJSON, function(err){
-          console.log(err)
-        } )
-      })
-     });
- })
-
+var fullJSON=[]
+var counter = 0;
+function writeFile(fullJSON){
+  fs.writeFile('./myjsonfile.json',JSON.stringify(fullJSON), function(err){
+    if (err) throw err;
+    console.log('The file has been saved in "data" folder!');
+  } )
 }
-
-start()
+ extract(process.env.DIR, {dir: __dirname+'/data/'}, function (err) { 
+  const directoryPath = path.join(__dirname, 'data');
+  fs.readdir(directoryPath, async function (err, files) {
+      if (err) {
+          return console.log('Unable to scan directory: ' + err);
+      }                    
+      files.forEach(function (file) { 
+        csv({
+          delimiter:'||',
+          colParser:{
+            'date':function(item, head, resultRow, row , colIdx){
+              try{var date=new Date(item).toISOString().substring(0, 10);}catch(err){}
+              return date;
+            },
+            'name':function(item, head, resultRow, row , colIdx){
+              return resultRow.last_name+' '+resultRow.first_name;
+            },
+            'phone':function(item, head, resultRow, row , colIdx){
+              return item.match( numberPattern ).join([]) ;
+              
+            },
+            'cc':function(item, head, resultRow, row , colIdx){
+              return item.substring(3, item.length)
+            },
+          },
+          ignoreColumns:/(user|email)/,
+        })
+        .fromFile('./data/'+file)
+        .then((jsonObj)=>{
+            for (var i=0; i<jsonObj.length; i++){
+              jsonObj[i]={
+                "name": jsonObj[i].name,
+                "phone": jsonObj[i].phone,
+                "person": {
+                  "firstName": jsonObj[i].first_name,
+                  "lastName":jsonObj[i].last_name
+                },
+                "amount": jsonObj[i].amount,
+                "date": jsonObj[i].date,
+                "costCenterNum": jsonObj[i].cc
+              }
+              fullJSON.push(jsonObj[i])
+              
+            }
+            counter = counter + 1;
+            if (files.length === counter) {
+              writeFile(fullJSON)
+            }
+        }) 
+      })  
+  })
+})
